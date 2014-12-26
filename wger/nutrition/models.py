sbvhev/wger

commit d0f666352e0a798105252fc87f21268d53e7f82b

Use __str__ instead of __unicode__ in models
This is more in line to django suggestion to write code for python 3 and add
python 2 as a backward compatibility.

wger/nutrition/models.py
@@ -28,6 +28,7 @@ from django.core.urlresolvers import reverse
 from django.core import mail
 from django.core.cache import cache
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.utils import translation
 
@@ -57,6 +58,7 @@ Simple approximation of energy (kcal) provided per gram or ounce
 logger = logging.getLogger('wger.custom')
 
 
+@python_2_unicode_compatible
 class NutritionPlan(models.Model):
     '''
     A nutrition plan
@@ -88,7 +90,7 @@ class NutritionPlan(models.Model):
                                                         "value yourself."))
     '''A flag indicating whether the plan has a goal amount of calories'''
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -181,6 +183,7 @@ class NutritionPlan(models.Model):
             return 4
 
 
+@python_2_unicode_compatible
 class Ingredient(AbstractLicenseModel, models.Model):
     '''
     An ingredient, with some approximate nutrition values
@@ -355,11 +358,11 @@ class Ingredient(AbstractLicenseModel, models.Model):
         super(Ingredient, self).save(*args, **kwargs)
         cache.delete(cache_mapper.get_ingredient_key(self.id))
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
-        return u"{0}".format(self.name)
+        return self.name
 
     def __eq__(self, other):
         '''
@@ -422,6 +425,7 @@ class Ingredient(AbstractLicenseModel, models.Model):
         return False
 
 
+@python_2_unicode_compatible
 class WeightUnit(models.Model):
     '''
     A more human usable weight unit (spoon, table, slice...)
@@ -438,9 +442,6 @@ class WeightUnit(models.Model):
         ordering = ["name", ]
 
     def __str__(self):
-        return self.__unicode__()
-
-    def __unicode__(self):
         '''
         Return a more human-readable representation
         '''
@@ -453,6 +454,7 @@ class WeightUnit(models.Model):
         return None
 
 
+@python_2_unicode_compatible
 class IngredientWeightUnit(models.Model):
     '''
     A specific human usable weight unit for an ingredient
@@ -476,7 +478,7 @@ class IngredientWeightUnit(models.Model):
         '''
         return None
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -486,6 +488,7 @@ class IngredientWeightUnit(models.Model):
                                        self.gram)
 
 
+@python_2_unicode_compatible
 class Meal(models.Model):
     '''
     A meal
@@ -506,7 +509,7 @@ class Meal(models.Model):
                           blank=True,
                           verbose_name=_('Time (approx)'))
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -547,6 +550,7 @@ class Meal(models.Model):
         return nutritional_info
 
 
+@python_2_unicode_compatible
 class MealItem(models.Model):
     '''
     An item (component) of a meal
@@ -572,7 +576,7 @@ class MealItem(models.Model):
                                  validators=[MinValueValidator(1),
                                              MaxValueValidator(1000)])
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''

wger/weight/models.py
@@ -19,9 +19,11 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 from django.contrib.auth.models import User
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
+@python_2_unicode_compatible
 class WeightEntry(models.Model):
     '''
     Model for a weight point
@@ -52,7 +54,7 @@ class WeightEntry(models.Model):
         get_latest_by = "creation_date"
         unique_together = ("creation_date", "user")
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''

wger/core/tests/test_license.py
@@ -15,11 +15,25 @@
 
 from wger.core.models import License
 from wger.core.tests import api_base_test
+from wger.exercises.models import Exercise
 
-from wger.manager.tests.testcase import WorkoutManagerAccessTestCase
+from wger.manager.tests.testcase import WorkoutManagerAccessTestCase, WorkoutManagerTestCase
 from wger.manager.tests.testcase import WorkoutManagerDeleteTestCase
 from wger.manager.tests.testcase import WorkoutManagerEditTestCase
 from wger.manager.tests.testcase import WorkoutManagerAddTestCase
+
+
+class LicenseRepresentationTestCase(WorkoutManagerTestCase):
+    '''
+    Test the representation of a model
+    '''
+
+    def test_representation(self):
+        '''
+        Test that the representation of an object is correct
+        '''
+        self.assertEqual("{0}".format(Exercise.objects.get(pk=1)),
+                         'A cool and free license - Germany (ACAFL - DE)')
 
 
 class LicenseOverviewTest(WorkoutManagerAccessTestCase):

wger/nutrition/tests/test_plan.py
@@ -22,6 +22,23 @@ from wger.manager.tests.testcase import WorkoutManagerEditTestCase
 from wger.manager.tests.testcase import WorkoutManagerTestCase
 
 
+class PlanRepresentationTestCase(WorkoutManagerTestCase):
+    '''
+    Test the representation of a model
+    '''
+
+    def test_representation(self):
+        '''
+        Test that the representation of an object is correct
+        '''
+        p = NutritionPlan.objects.get(pk=1)
+        self.assertEqual("{0}".format(p), 'Description 1')
+
+        p.description = ''
+        p.save()
+        self.assertEqual("{0}".format(p), 'Nutrition plan')
+
+
 class DeletePlanTestCase(WorkoutManagerDeleteTestCase):
     '''
     Tests deleting a nutritional plan

wger/weight/tests/test_entry.py
@@ -20,8 +20,20 @@ from wger.utils.constants import TWOPLACES
 
 from wger.weight.models import WeightEntry
 
-from wger.manager.tests.testcase import WorkoutManagerEditTestCase
+from wger.manager.tests.testcase import WorkoutManagerEditTestCase, WorkoutManagerTestCase
 from wger.manager.tests.testcase import WorkoutManagerAddTestCase
+
+
+class MealRepresentationTestCase(WorkoutManagerTestCase):
+    '''
+    Test the representation of a model
+    '''
+
+    def test_representation(self):
+        '''
+        Test that the representation of an object is correct
+        '''
+        self.assertEqual("{0}".format(WeightEntry.objects.get(pk=1)), '2012-10-01: 77kg')
 
 
 class AddWeightEntryTestCase(WorkoutManagerAddTestCase):

wger/config/models.py
@@ -19,6 +19,7 @@ import logging
 
 from django.db import models
 from django.db.models.signals import post_save
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from django.core.cache import cache
 from wger.core.models import Language
@@ -33,6 +34,7 @@ from wger.utils.cache import cache_mapper
 logger = logging.getLogger('wger.custom')
 
 
+@python_2_unicode_compatible
 class LanguageConfig(models.Model):
     '''
     Configuration for languages
@@ -63,7 +65,7 @@ class LanguageConfig(models.Model):
         '''
         ordering = ["item", "language_target", ]
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -122,6 +124,7 @@ def init_language_config(sender, instance, created, **kwargs):
 post_save.connect(init_language_config, sender=Language)
 
 
+@python_2_unicode_compatible
 class GymConfig(models.Model):
     '''
     System wide configuration for gyms
@@ -142,7 +145,7 @@ class GymConfig(models.Model):
     Default gym for the wger installation
     '''
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''

wger/core/models.py
@@ -24,6 +24,7 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 from wger.gym.models import Gym
 
@@ -34,6 +35,7 @@ from wger.utils.units import AbstractWeight
 from wger.weight.models import WeightEntry
 
 
+@python_2_unicode_compatible
 class Language(models.Model):
     '''
     Language of an item (exercise, workout, etc.)
@@ -56,7 +58,7 @@ class Language(models.Model):
     #
     # Django methods
     #
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -78,6 +80,7 @@ class Language(models.Model):
         return False
 
 
+@python_2_unicode_compatible
 class UserProfile(models.Model):
     GENDER_MALE = '1'
     GENDER_FEMALE = '2'
@@ -317,7 +320,7 @@ by the US Department of Agriculture. It is extremely complete, with around
            and (self.sleep_hours + self.freetime_hours + self.work_hours) > 24):
                 raise ValidationError(_('The sum of all hours has to be 24'))
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -449,6 +452,7 @@ def create_user_profile(sender, instance, created, **kwargs):
 post_save.connect(create_user_profile, sender=User)
 
 
+@python_2_unicode_compatible
 class DaysOfWeek(models.Model):
     '''
     Model for the days of the week
@@ -465,13 +469,14 @@ class DaysOfWeek(models.Model):
         '''
         ordering = ["pk", ]
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
         return self.day_of_week
 
 
+@python_2_unicode_compatible
 class License(models.Model):
     '''
     License for an item (exercise, ingredient, etc.)
@@ -500,7 +505,7 @@ class License(models.Model):
     #
     # Django methods
     #
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''

wger/exercises/models.py
@@ -23,6 +23,7 @@ from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify  # django.utils.text.slugify in django 1.5!
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils import translation
 from django.core.urlresolvers import reverse
 from django.core import mail
@@ -47,6 +48,7 @@ from wger.utils.cache import cache_mapper
 logger = logging.getLogger('wger.custom')
 
 
+@python_2_unicode_compatible
 class Muscle(models.Model):
     '''
     Muscle an exercise works out
@@ -63,7 +65,7 @@ class Muscle(models.Model):
     class Meta:
         ordering = ["name", ]
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -76,6 +78,7 @@ class Muscle(models.Model):
         return False
 
 
+@python_2_unicode_compatible
 class Equipment(models.Model):
     '''
     Equipment used or needed by an exercise
@@ -90,7 +93,7 @@ class Equipment(models.Model):
         '''
         ordering = ["name", ]
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -103,6 +106,7 @@ class Equipment(models.Model):
         return False
 
 
+@python_2_unicode_compatible
 class ExerciseCategory(models.Model):
     '''
     Model for an exercise category
@@ -115,7 +119,7 @@ class ExerciseCategory(models.Model):
         verbose_name_plural = _("Exercise Categories")
         ordering = ["name", ]
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -150,6 +154,7 @@ class ExerciseCategory(models.Model):
         super(ExerciseCategory, self).delete(*args, **kwargs)
 
 
+@python_2_unicode_compatible
 class Exercise(AbstractSubmissionModel, AbstractLicenseModel, models.Model):
     '''
     Model for an exercise
@@ -257,7 +262,7 @@ class Exercise(AbstractSubmissionModel, AbstractLicenseModel, models.Model):
 
         super(Exercise, self).delete(*args, **kwargs)
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''
@@ -495,6 +500,7 @@ pre_save.connect(delete_exercise_image_on_update, sender=ExerciseImage)
 saved_file.connect(generate_aliases)
 
 
+@python_2_unicode_compatible
 class ExerciseComment(models.Model):
     '''
     Model for an exercise comment
@@ -506,7 +512,7 @@ class ExerciseComment(models.Model):
                                verbose_name=_('Comment'),
                                help_text=_('A comment about how to correctly do this exercise.'))
 
-    def __unicode__(self):
+    def __str__(self):
         '''
         Return a more human-readable representation
         '''

commit 474d5bda82f4a1da324d2de3fbbdc455f8aea07b

Fix some dumb errors introduced in d0f666352

wger/nutrition/models.py
@@ -95,9 +95,9 @@ class NutritionPlan(models.Model):
         Return a more human-readable representation
         '''
         if self.description:
-            return self.description
+            return u"{0}".format(self.description)
         else:
-            return unicode(_("Nutrition plan"))
+            return u"{0}".format(_("Nutrition plan"))
 
     def get_absolute_url(self):
         '''

wger/weight/models.py
@@ -58,7 +58,7 @@ class WeightEntry(models.Model):
         '''
         Return a more human-readable representation
         '''
-        return u"{0}: {1} kg".format(self.creation_date, self.weight)
+        return u"{0}: {1:.2f} kg".format(self.creation_date, self.weight)
 
     def get_owner_object(self):
         '''

wger/core/tests/test_license.py
@@ -15,7 +15,6 @@
 
 from wger.core.models import License
 from wger.core.tests import api_base_test
-from wger.exercises.models import Exercise
 
 from wger.manager.tests.testcase import WorkoutManagerAccessTestCase, WorkoutManagerTestCase
 from wger.manager.tests.testcase import WorkoutManagerDeleteTestCase
@@ -32,7 +31,7 @@ class LicenseRepresentationTestCase(WorkoutManagerTestCase):
         '''
         Test that the representation of an object is correct
         '''
-        self.assertEqual("{0}".format(Exercise.objects.get(pk=1)),
+        self.assertEqual("{0}".format(License.objects.get(pk=1)),
                          'A cool and free license - Germany (ACAFL - DE)')
 
 

wger/nutrition/tests/test_plan.py
@@ -31,7 +31,7 @@ class PlanRepresentationTestCase(WorkoutManagerTestCase):
         '''
         Test that the representation of an object is correct
         '''
-        p = NutritionPlan.objects.get(pk=1)
+        p = NutritionPlan.objects.get(pk=5)
         self.assertEqual("{0}".format(p), 'Description 1')
 
         p.description = ''

wger/weight/tests/test_entry.py
@@ -33,7 +33,7 @@ class MealRepresentationTestCase(WorkoutManagerTestCase):
         '''
         Test that the representation of an object is correct
         '''
-        self.assertEqual("{0}".format(WeightEntry.objects.get(pk=1)), '2012-10-01: 77kg')
+        self.assertEqual("{0}".format(WeightEntry.objects.get(pk=1)), '2012-10-01: 77.00 kg')
 
 
 class AddWeightEntryTestCase(WorkoutManagerAddTestCase):